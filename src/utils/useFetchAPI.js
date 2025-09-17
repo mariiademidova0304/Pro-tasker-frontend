f77d8548e300a03401bc1de4c8b66f143f6f6143
Mariia D <mariia.demidova0304@gmail.com>
2025-09-17 15:33:24 -0400

    Initial commit + added fetchAPi hook from a previous project, it needs tweeking for JWT

---
+++ src/utils/useFetchAPI.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+
+//NEED TO ADD JWT TOKEN
+//remember this hook from lesson material, tried to really understand what it all means
+export default function useFetchAPI(url) {
+    const [apiData, setApiData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+//if url is not full yet don't do anything to nit get errors
+        if (!url) return;
+
+        const controller = new AbortController;
+        setApiData(null);
+        setLoading(true);
+        setError(null);
+
+        const fetchAPI = async () => {
+            try {
+                //setting up a signal that, when cleanup suddenly runs on updated url, controller cancels this request
+                const response = await fetch(url, {signal: controller.signal});
+                if (!response.ok) {
+                    throw new Error(`Error! Could not get response due to: ${response.status}`)
+                }
+                const result = await response.json();
+                setApiData(result);
+            } catch (error) {
+                //since we manually set up cancelling of the request with controller, we don't need this error
+                if (error.name !== 'AbortError') {
+                    setError(error);
+                }
+            } finally {
+                setLoading(false);
+            }
+
+        }
+
+        fetchAPI();
+
+        return () => {
+            controller.abort();
+        }
+    }, [url])
+
+    return {apiData, loading, error};
+
+}
