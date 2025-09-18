4f58216cbf03ca023253d0ce4f618c433061a498
Mariia D <mariia.demidova0304@gmail.com>
2025-09-18 13:56:19 -0400

    Auth ContextAPI should be working for login and logout

--- src/context/ContextAPI.jsx
+++ src/context/ContextAPI.jsx
@@ -11,11 +11,21 @@ export function currentUserProvider({ children }) {
         const jwtFromStorage = JSON.parse(localStorage.getItem('jwt'));
         return jwtFromStorage ? jwtFromStorage : null;
     });
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     const login = async (email, password) => {
         try {
+            const requestOptions = {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    'email' : email,
+                    'password': password
+                })
+            }
             //setting up a signal that, when cleanup suddenly runs on updated url, controller cancels this request
-            const response = await fetch(url);
+            const response = await fetch(`${import.meta.env.VITE_SERVER_ORIGIN}/api/users/login`, requestOptions);
             if (!response.ok) {
                 throw new Error(`Error! Could not get response due to: ${response.status}`)
             }
@@ -24,6 +34,9 @@ export function currentUserProvider({ children }) {
             localStorage.setItem('jwt', JSON.stringify(result.token));
         } catch (error) {
             console.log(error.message);
+            setError(error);
+        } finally{
+            setLoading(false);
         }
     }
 
@@ -32,7 +45,7 @@ export function currentUserProvider({ children }) {
         localStorage.removeItem('jwt');
     }
 
-    const jwtValue = useMemo(() => ({ jwt, login, logout }), [jwt]);
+    const jwtValue = useMemo(() => ({ jwt, login, logout, error, loading }), [jwt, error, loading]);
 
     return (
         <currentUserContext.Provider value={jwtValue}>
